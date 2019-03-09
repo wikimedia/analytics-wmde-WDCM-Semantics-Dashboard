d109605e376e15966255dd30dcd7cca5c1cff636
GoranMilovanovic <goran.s.milovanovic@gmail.com>
2019-03-09 22:23:59 +0100

    client-side full dependency
    Change-Id: Ib050331dd9c7033e273a7b21fd0b53f0b56e53b5

--- server.R
+++ server.R
@@ -27,6 +27,9 @@ library(ggplot2)
 library(ggrepel)
 library(scales)
 library(rbokeh)
+### --- connect
+library(httr)
+library(curl)
 
 ### --- Server (Session) Scope
 ### --------------------------------
@@ -35,144 +38,146 @@ library(rbokeh)
 params <- xmlParse('wdcmConfig_wdcmSemanticsDashboard.xml')
 params <- xmlToList(params)
 
-### --- Credentials
-# - credentials on tools.labsdb
-cred <- readLines(params$mySQL_Credentials_Path)
-mySQLCreds <- data.frame(user = gsub("^[[:alnum:]]+\\s=\\s", "", cred[2]),
-                         password = gsub("^[[:alnum:]]+\\s=\\s", "", cred[3]),
-                         stringsAsFactors = F)
-rm(cred)
-
-
-### -- Connect
-con <- dbConnect(MySQL(), 
-                 host = "tools.labsdb", 
-                 default.file = "/home/goransm/mySQL_Credentials/replica.my.cnf",
-                 dbname = "u16664__wdcm_p",
-                 user = mySQLCreds$user,
-                 password = mySQLCreds$password)
-
-### --- list existing tables
-q <- "SHOW TABLES;"
-res <- dbSendQuery(con, q)
-st <- fetch(res, -1)
-dbClearResult(res)
-colnames(st) <- "tables"
-
-### --- SET CHARACTER SET utf8
-q <- "SET CHARACTER SET utf8;"
-res <- dbSendQuery(con, q)
-dbClearResult(res)
-
-### --- itemTopicTables
-itemTopicTables <- st$tables[which(grepl("wdcm2_itemtopic_", st$tables, fixed = T))]
-
-### --- fetch wdcm2_project
-q <- "SELECT * FROM wdcm2_project;"
-res <- dbSendQuery(con, q)
-wdcmProject <- fetch(res, -1)
-dbClearResult(res)
-colnames(wdcmProject) <- c('Project', 'Usage', 'Project Type')
-
-### --- fetch wdcm2_project_category
-q <- "SELECT * FROM wdcm2_project_category;"
-res <- dbSendQuery(con, q)
-wdcmProjectCategory <- fetch(res, -1)
-dbClearResult(res) 
-colnames(wdcmProjectCategory) <- c('Project', 'Category', 'Usage', 'Project Type')
-
-### --- fetch wdcm2_category
-q <- "SELECT * FROM wdcm2_category;"
-res <- dbSendQuery(con, q)
-wdcmCategory <- fetch(res, -1)
-dbClearResult(res) 
-colnames(wdcmCategory) <- c('Category', 'Usage')
-
-### --- fetch wdcm2_projects_2dmaps
-q <- "SELECT * FROM wdcm2_projects_2dmaps;"
-res <- dbSendQuery(con, q)
-wdcm2_projects_2dmaps <- fetch(res, -1)
-dbClearResult(res) 
-colnames(wdcm2_projects_2dmaps) <- c('D1', 'D2', 'Project', 'Project Type', 'Category')
-
-### --- Disconnect
-dbDisconnect(con)
-
-### --- Fetch local files
-setwd(params$localFilesPath)
-
-### --- fetch projecttopic tables
-lF <- list.files()
-lF <- lF[grepl("wdcm2_projecttopic_", lF, fixed = T)]
-projectTopic <- vector(mode = "list", length = length(lF))
-for (i in 1:length(lF)) {
-  projectTopic[[i]] <- fread(lF[i], data.table = F)
+### --- functions
+get_WDCM_table <- function(url_dir, filename, row_names) {
+  read.csv(paste0(url_dir, filename), 
+           header = T, 
+           stringsAsFactors = F,
+           check.names = F)
 }
-names(projectTopic) <- sapply(lF, function(x) {
-  strsplit(strsplit(x, split = ".", fixed = T)[[1]][1],
-           split = "_",
-           fixed = T)[[1]][3]
-})
 
-### --- fetch wdcm2_networkNodes_project tables
-lF <- list.files()
-lF <- lF[grepl("wdcm2_visNetworkNodes_project", lF, fixed = T)]
-networkNodes <- vector(mode = "list", length = length(lF))
-for (i in 1:length(lF)) {
-  networkNodes[[i]] <- fread(lF[i])
+# - projectType() to determine project type
+projectType <- function(projectName) {
+  unname(sapply(projectName, function(x) {
+    if (grepl("commons", x, fixed = T)) {"Commons"
+    } else if (grepl("mediawiki|meta|species|wikidata", x)) {"Other"
+    } else if (grepl("wiki$", x)) {"Wikipedia"
+    } else if (grepl("quote$", x)) {"Wikiquote"
+    } else if (grepl("voyage$", x)) {"Wikivoyage"
+    } else if (grepl("news$", x)) {"Wikinews"
+    } else if (grepl("source$", x)) {"Wikisource"
+    } else if (grepl("wiktionary$", x)) {"Wiktionary"
+    } else if (grepl("versity$", x)) {"Wikiversity"
+    } else if (grepl("books$", x)) {"Wikibooks"
+    } else {"Other"}
+  }))
 }
-names(networkNodes) <- sapply(lF, function(x) {
-  strsplit(strsplit(x, split = ".", fixed = T)[[1]][1],
-           split = "_",
-           fixed = T)[[1]][4]
-})
-
-### --- fetch wdcm2_networkEdges_project tables
-lF <- list.files()
-lF <- lF[grepl("wdcm2_visNetworkEdges_project", lF, fixed = T)]
-networkEdges <- vector(mode = "list", length = length(lF))
-for (i in 1:length(lF)) {
-  networkEdges[[i]] <- fread(lF[i])
-}
-names(networkEdges) <- sapply(lF, function(x) {
-  strsplit(strsplit(x, split = ".", fixed = T)[[1]][1],
-           split = "_",
-           fixed = T)[[1]][4]
-})
-
-### --- Fetch update info
-update <- read.csv(params$update_File_Path, 
-                   header = T,
-                   check.names = F,
-                   stringsAsFactors = F,
-                   row.names = 1)
-
-### - Determine Constants
-# - determine Projects
-projects <- wdcmProject$Project
-# - determine present Project Types
-projectTypes <- unique(wdcmProject$`Project Type`)
-# - and assign Brewer colors
-lengthProjectColor <- length(unique(wdcmProject$`Project Type`))
-projectTypeColor <- brewer.pal(lengthProjectColor, "Set1")
-names(projectTypeColor) <- unique(wdcmProject$`Project Type`)
-# - determine Categories
-categories <- wdcmCategory$Category
-# - totalUsage
-totalUsage <- sum(wdcmProject$Usage)
-totalProjects <- length(wdcmProject$Project)
-totalCategories <- length(wdcmCategory$Category)
-totalProjectTypes <- length(unique(wdcmProject$`Project Type`))
-
-### --- prepare search constants for Tabs/Crosstabs
-search_projectTypes <- paste("_", projectTypes, sep = "")
-unzip_projectTypes <- lapply(projectTypes, function(x) {
-  wdcmProject$Project[which(wdcmProject$`Project Type` %in% x)]
-})
-names(unzip_projectTypes) <- search_projectTypes
 
 ### --- shinyServer
 shinyServer(function(input, output, session) {
+  
+  ### --- DATA
+  
+  ### --- list remote ml directory
+  
+  # - download ML and ETL files:
+  withProgress(message = 'Downloading data', detail = "Please be patient.", value = 0, {
+    
+    # - list files:
+    incProgress(1/8, detail = "Access data repository.")
+    url <- params$remoteMLDir
+    page <- as.character(GET(url), config = httr::config(http_version = 2))
+    links <- str_extract_all(page, "<a href=.+>.+</a>")
+    links <- sapply(links, function(x) {str_extract_all(x, ">.+<")})
+    links <- sapply(links, function(x) {gsub('^>|"|<$|>|<', "", x)})
+    links <- links[3:length(links)]
+    tSNEmapsFiles <- links[which(grepl("wdcm2_tsne2D_project_", links, fixed = T))]
+    itemTopicTables <- links[which(grepl("wdcm2_itemtopic_", links, fixed = T))]
+    projectTopicTables <- links[which(grepl("wdcm2_projecttopic_", links, fixed = T))]
+
+    ### --- fetch wdcm2_project
+    incProgress(2/8, detail = "ETL dataset.")
+    wdcmProject <- get_WDCM_table(params$remoteETLDir, 'wdcm_project.csv')
+    wdcmProject$type <- projectType(wdcmProject$eu_project)
+    colnames(wdcmProject) <- c('Project', 'Usage', 'Project Type')
+    
+    ### --- fetch 	wdcm_category_item.csv
+    incProgress(3/8, detail = "ETL dataset.")
+    wdcm_category_item <- get_WDCM_table(
+      params$remoteETLDir, 'wdcm_category_item.csv')
+    colnames(wdcm_category_item) <- c('Entity', 'Usage', 'Label', 'Category')
+    
+    ### --- fetch wdcm2_project_category
+    incProgress(4/8, detail = "ETL dataset.")
+    wdcmProjectCategory <- get_WDCM_table(params$remoteETLDir, 'wdcm_project_category.csv', row_names = F)
+    wdcmProjectCategory$type <- projectType(wdcmProjectCategory$eu_project)
+    colnames(wdcmProjectCategory) <- c('Project', 'Category', 'Usage', 'Project Type')
+    # - fix `Wikimedia_Internal` to `Wikimedia`
+    wdcmProjectCategory$Category[wdcmProjectCategory$Category == 'Wikimedia_Internal'] <- 
+      'Wikimedia'
+
+    ### --- fetch wdcm2_category
+    incProgress(5/8, detail = "ETL dataset.")
+    wdcmCategory <- get_WDCM_table(params$remoteETLDir, 'wdcm_category.csv')
+    colnames(wdcmCategory) <- c('Category', 'Usage')
+    # - fix `Wikimedia_Internal` to `Wikimedia`
+    wdcmCategory$Category[wdcmCategory$Category == 'Wikimedia_Internal'] <- 
+      'Wikimedia'
+    
+    ### --- fetch wdcm2_projects_2dmaps
+    incProgress(6/8, detail = "tSNE Semantic Maps.")
+    wdcm2_projects_2dmaps <- lapply(tSNEmapsFiles, function(x) {
+      get_WDCM_table(url_dir = params$remoteMLDir, x, row_names = T)
+    })
+    # - enter category info:
+    for (i in 1:length(tSNEmapsFiles)) {
+      wdcm2_projects_2dmaps[[i]]$Category <- gsub(".csv", "",
+        strsplit(tSNEmapsFiles[i], "_")[[1]][4], fixed = T)
+    }
+    wdcm2_projects_2dmaps <- rbindlist(wdcm2_projects_2dmaps)
+    wdcm2_projects_2dmaps$V1 <- NULL
+    colnames(wdcm2_projects_2dmaps) <- c('D1', 'D2', 'Project', 'Project Type', 'Category')
+    wdcm2_projects_2dmaps$Category[wdcm2_projects_2dmaps$Category == "Wikimedia_Internal"] <- 
+      'Wikimedia'
+    
+    ### --- fetch projectTopic tables
+    incProgress(7/8, detail = "Projects data.")
+    projectTopic <- lapply(projectTopicTables, function(x) {
+      d <- get_WDCM_table(url_dir = params$remoteMLDir, x, row_names = T)
+      colnames(d)[1] <- 'id'
+      d
+    })
+    names(projectTopic) <- sapply(projectTopicTables, function(x) {
+      strsplit(strsplit(x, split = ".", fixed = T)[[1]][1],
+               split = "_",
+               fixed = T)[[1]][3]
+    })
+    
+    ### --- Fetch update info
+    incProgress(8/8, detail = "Update info.")
+    update <- read.csv(params$update_File_Path,
+                       header = T,
+                       check.names = F,
+                       stringsAsFactors = F,
+                       row.names = 1)
+    })
+
+  ### - Determine Constants
+  # - determine Projects
+  projects <- wdcmProject$Project
+  # - determine present Project Types
+  projectTypes <- unique(wdcmProject$`Project Type`)
+  # - and assign Brewer colors
+  lengthProjectColor <- length(unique(wdcmProject$`Project Type`))
+  projectTypeColor <- brewer.pal(lengthProjectColor, "Set1")
+  names(projectTypeColor) <- unique(wdcmProject$`Project Type`)
+  # - determine Categories
+  categories <- wdcmCategory$Category
+  # - totalUsage
+  totalUsage <- sum(wdcmProject$Usage)
+  totalProjects <- length(wdcmProject$Project)
+  totalCategories <- length(wdcmCategory$Category)
+  totalProjectTypes <- length(unique(wdcmProject$`Project Type`))
+  
+  ### --- prepare search constants for Tabs/Crosstabs
+  search_projectTypes <- paste("_", projectTypes, sep = "")
+  unzip_projectTypes <- lapply(projectTypes, function(x) {
+    wdcmProject$Project[which(wdcmProject$`Project Type` %in% x)]
+  })
+  names(unzip_projectTypes) <- search_projectTypes
+  
+  
+  ### --- OUTPUTS
   
   ### --- output: updateString
   output$updateString <- renderText({
@@ -184,14 +189,7 @@ shinyServer(function(input, output, session) {
   ### ------------------------------------------
   ### --- TAB: tabPanel Semantic Models
   ### ------------------------------------------
-  
-  ### --- output: updateString
-  output$updateString <- renderText({
-    date <- update[max(which(grepl("Orchestra END", update$Step))), ]$Time
-    date <- paste0(date, " UTC")
-    return(paste('<p style="font-size:80%;"align="right"><b>Last update: </b><i>', date, '</i></p>', sep = ""))
-  })
-  
+
   ### --- SELECT: update select 'selectCategory'
   updateSelectizeInput(session,
                        'selectCategory',
@@ -200,28 +198,26 @@ shinyServer(function(input, output, session) {
                        selected = categories[round(runif(1, 1, length(categories)))],
                        server = TRUE)
   
-  ### --- REACTIVE: category specific wdcm_itemtopic data.frame
+  ### --- REACTIVE itemTopic: current items x topics matrix (category-specific)
+  itemTopic <- reactive({
+    withProgress(message = 'Downloading data', detail = "Items x Topics Matrix", value = 0, {
+      incProgress(1/2, detail = "Items x Topics Matrix")
+      dFile <- itemTopicTables[which(grepl(input$selectCategory, itemTopicTables))]
+      incProgress(2/2, detail = "Items x Topics Matrix")
+      iT <- get_WDCM_table(url_dir = params$remoteMLDir, dFile, row_names = T)
+      colnames(iT)[1] <- "Entity"
+      iT
+    })
+  })
+  
+  ### --- REACTIVE itemTopicsNum: how many topics in a given category?
   itemTopicsNum <- reactive({
-    sC <- gsub(" ", "", input$selectCategory, fixed = T)
-    sTable <- itemTopicTables[which(grepl(sC, itemTopicTables, fixed = T))]
-    ### -- Connect
-    con <- dbConnect(MySQL(), 
-                     host = "tools.labsdb", 
-                     default.file = "/home/goransm/mySQL_Credentials/replica.my.cnf",
-                     dbname = "u16664__wdcm_p",
-                     user = mySQLCreds$user,
-                     password = mySQLCreds$password)
-    ### --- check the particular table
-    q <- paste("DESCRIBE ", sTable, ";", sep = "")
-    res <- dbSendQuery(con, q)
-    sIT <- fetch(res, -1)
-    dbClearResult(res)
-    ### --- Disconnect
-    dbDisconnect(con)
-    sum(grepl("topic", sIT$Field))
+    tops <- colnames(itemTopic())
+    sum(grepl("topic", tops))
   })
   
   ### --- SELECT: updateSelectizeInput 'selectCatTopic'
+  ### --- select a particular topic from a category-specific LDA model
   output$selectCatTopic <-
     renderUI({
       if ((is.null(input$selectCategory)) | (length(input$selectCategory) == 0)) {
@@ -238,42 +234,16 @@ shinyServer(function(input, output, session) {
       }
     })
   
-  ### --- REACTIVE current itemTopic table:
-  itemTopic <- reactive({
-      sC <- gsub(" ", "", input$selectCategory, fixed = T)
-      sTable <- itemTopicTables[which(grepl(sC, itemTopicTables, fixed = T))]
-      cTopic <- tolower(gsub(" ", "", input$selectCategoryTopic))
-      if (!(length(cTopic) == 0)) {
-        ### -- Connect
-        con <- dbConnect(MySQL(),
-                         host = "tools.labsdb",
-                         default.file = "/home/goransm/mySQL_Credentials/replica.my.cnf",
-                         dbname = "u16664__wdcm_p",
-                         user = mySQLCreds$user,
-                         password = mySQLCreds$password)
-        ### --- check the particular table
-        q <- 'SET CHARACTER SET utf8;'
-        res <- dbSendQuery(con, q)
-        q <- paste("SELECT * FROM ", sTable, " ORDER BY ", cTopic, " DESC LIMIT 50;", sep = "")
-        res <- dbSendQuery(con, q)
-        iT <- fetch(res, -1)
-        dbClearResult(res)
-        ### --- Disconnect
-        dbDisconnect(con)
-        ### --- Output:
-        return(iT) 
-      } else {return(NULL)}
-  })
-  
   ### --- OUTPUT output$topItemsTopic
   output$topItemsTopic <- renderPlot({
     if (!is.null(itemTopic())) {
       cTopic <- tolower(gsub(" ", "", input$selectCategoryTopic))
-      plotFrame <- itemTopic()
-      plotFrame <- select(plotFrame, 
-                          eu_label, eu_entity_id, cTopic)
-      colnames(plotFrame) <- c('Label', 'Id', 'Probability')
-      plotFrame$Label <- paste(1:dim(plotFrame)[1], ". ", plotFrame$Label, sep = "")
+      plotFrame <- as.data.frame(itemTopic()$Entity)
+      colnames(plotFrame) <- 'Entity'
+      plotFrame <- cbind(plotFrame, select(itemTopic(), cTopic))
+      colnames(plotFrame) <- c('Id', 'Probability')
+      plotFrame <- arrange(plotFrame, desc(Probability)) %>% head(50)
+      plotFrame$Label <- paste(1:dim(plotFrame)[1], ". ", plotFrame$Id, sep = "")
       plotFrame$Label <- factor(plotFrame$Label, 
                                 levels = plotFrame$Label[order(plotFrame$Probability)])
       plotFrame$Sign <- paste("(", 1:dim(plotFrame)[1], ") ", plotFrame$Id, sep = "")
@@ -301,12 +271,23 @@ shinyServer(function(input, output, session) {
   output$networkItemsTopic <- renderVisNetwork({
     
     if (!is.null(itemTopic())) {
-      # - normalization: Luce's choice axiom
-      itemNames <- itemTopic()$eu_entity_id
+      cTopic <- tolower(gsub(" ", "", input$selectCategoryTopic))
+      w <- which(colnames(itemTopic()) == cTopic)
+      itemNames <- itemTopic()[, c(1, w)]
+      itemNames <- itemNames[order(-itemNames[, 2]), ]
+      itemNames <- itemNames$Entity
       root <- dplyr::select(itemTopic(), starts_with('topic'))
+      selItems <- wdcm_category_item %>% 
+        filter(grepl(input$selectCategory, wdcm_category_item$Category)) %>% 
+        arrange(desc(Usage)) %>% 
+        head(50) %>% 
+        select(Entity, Label)
+      w <- which(itemNames %in% selItems$Entity)
+      root <- root[w, ]
       root <- as.matrix(parDist(as.matrix(root), method = "euclidean"))
-      rownames(root) <- itemNames
-      colnames(root) <- itemNames
+      w <- which(selItems$Entity %in% itemNames)
+      rownames(root) <- paste0(selItems$Label[w], " (", selItems$Entity[w], ")") 
+      colnames(root) <- paste0(selItems$Label[w], " (", selItems$Entity[w], ")")
       indexMinDist <- sapply(rownames(root), function(x) {
         w <- which(rownames(root) %in% x)
         y <- sort(root[w, -w], decreasing = T)
@@ -327,9 +308,9 @@ shinyServer(function(input, output, session) {
         nodes$id[which(nodes$label %in% x)]
       })
       conceptsStruct$arrows <- rep("to", length(conceptsStruct$to))
-      nodes$label <- sapply(nodes$label, function(x) {
-        itemTopic()$eu_label[itemTopic()$eu_entity_id == x]
-      })
+      # nodes$label <- sapply(nodes$label, function(x) {
+      #   itemTopic()$eu_label[itemTopic()$eu_entity_id == x]
+      # })
       visNetwork(nodes = nodes,
                  edges = conceptsStruct,
                  width = "100%",
@@ -346,9 +327,16 @@ shinyServer(function(input, output, session) {
   
   ### --- REACTIVE current pTopic data.frame:
   pTopic <- reactive({
-    w <- which(names(projectTopic) %in% gsub(" ", "", input$selectCategory, fixed = T))
-    if (!length(w) == 0) {
-      pTopic <- as.data.frame(projectTopic[[w]])
+    
+    withProgress(message = 'Downloading data', detail = "Projects x Topics Matrix", value = 0, {
+      incProgress(1/2, detail = "Projects x Topics Matrix")
+      dFile <- projectTopicTables[which(grepl(input$selectCategory, projectTopicTables))]
+      incProgress(2/2, detail = "Projects x Topics Matrix")
+      pTopic <- get_WDCM_table(url_dir = params$remoteMLDir, dFile, row_names = T)
+      colnames(pTopic)[1] <- "Project"
+    })
+    
+    if (!is.null(pTopic)) {
       cTopic <- which(colnames(pTopic) %in% tolower(gsub(" ", "", input$selectCategoryTopic)))
       if (!length(cTopic) == 0) {
         pTopic <- pTopic %>% 
@@ -464,7 +452,7 @@ shinyServer(function(input, output, session) {
                  label = paste(Proportion, "%", sep = ""))
              ) +
         geom_line(group = 1, size = .25, color = "#4c8cff") +
-        geom_point(size = 1.5, color ="#4c8cff", fill = "#4c8cff") + 
+        geom_point(size = 1.5, color = "#4c8cff", fill = "#4c8cff") + 
         geom_point(size = 1, color = "white") + 
         geom_text_repel(size = 4) +
         facet_wrap(~ Category, ncol = 3, scales = "free_y") +
